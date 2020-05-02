cff723f9a097d025f2af467a8eca342f2139d12e
Joseph <jwang@ucvts.org>
2020-05-02 10:06:50 -0400

    Trying to fix the java build path

--- cards/Cards.java
+++ cards/Cards.java
@@ -2,4 +2,18 @@ public class Cards {
   enum Suit {
     CLUB, DIAMOND, SPADE, HEART
   }
+  enum Value {
+    TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9),
+    TEN(10), JACK(11), KING(12), QUEEN(13), ACE(0);
+  }
+
+  Suit cardSuit;
+  Card cardValue;
+
+    public Card(Suit suit, Value value) {
+      this.value = value;
+      this.suit = suit;
+    }
+
+  }
 }
--- game/Launch.java
+++ game/Launch.java
@@ -1,3 +1,5 @@
+import cards.Cards.java;
+
 public class Launch {
   public static void main(String args[]) {
     System.out.println("Testing.");
